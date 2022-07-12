01ec13cef67418e58210cf208c869a1f6a9b079b
Amir Abdullaev <amir.abdullaev@emai.cz>
2022-07-12 22:05:08 +0200

    first codes on javascripts

---
+++ index.js
@@ -0,0 +1,12 @@
+// First code
+console.log('hello there! :D');
+
+
+let name='Amir';
+console.log(name);
+
+// canot be reserved keyword
+// should be meaningful
+// cannot start with a number(1name)
+// cannot cotain a space or hyphen (-)
+//are case-sensitive
