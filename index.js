526e192403af3d81c03b25ec0b024632b7b27bcd
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-03-28 12:48:47 +0100

    learning basics

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// Purpose: To test the connection between index.html and index.js
 function myFunction() {
   document.getElementById("index-2").style.fontSize="100px";
 }
+
+document.getElementById("p1").textContent = "Hello World!";
+
+// Variables
+let a = "Amir je frejer";
+console.log(a);
+
+let m, i, r;
+m = 19;
+i = 1554;
+r = 1.5;
+relationship = false; // Boolean
+console.log(`Amir is ${m} years old, Ivi ${i} years old, a Rijini ${r} godina.`);
+console.log("variable m is " + typeof m, "variable i is " + typeof i, "variable i is " + typeof r);
+console.log(`Is Amir in a relationship? ${relationship}`); 
