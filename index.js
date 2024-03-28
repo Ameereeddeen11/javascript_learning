577fc4021dee570e65454ced4e5d23b0542dfc3f
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-03-28 23:03:38 +0100

    create 2 mini projects

--- index.js
+++ index.js
@@ -19,9 +19,89 @@ console.log("variable m is " + typeof m, "variable i is " + typeof i, "variable 
 console.log(`Is Amir in a relationship? ${relationship}`); 
 
 document.getElementById("submit").onclick = function() {
-  wages = document.getElementById("wages").value;
-  const dph = document.getElementById("dph").value;
+  wages = Number(document.getElementById("wages").value);
+  const dph = Number(document.getElementById("dph").value);
   const insurance = 0.071;
-  income = wages - (wages * dph/100) - (wages * insurance);
-  document.getElementById("result").textContent = `Your income is ${income}!`;
+  if (validation(wages, dph) == false){
+    document.getElementById("result").textContent = "Please enter valid values!"
+  }
+  else{
+    income = wages - (wages * dph/100) - (wages * insurance);
+    document.getElementById("result").textContent = `Your income is ${income} !`;
+  }
+}
+
+function validation(wages, dph){
+  wages = Boolean(wages);
+  dph = Boolean(dph);
+  if (wages && dph) {
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function select(){
+  switch (document.getElementById("objects").value){
+    case "circle":
+      document.getElementById("circle-par").style.display = 'block'
+      break;
+    case "square":
+      document.getElementById("square-par").style.display = 'block'
+      break;
+    case "rectangle":
+      document.getElementById("rectangle-par").style.display = 'block'
+      break;
+    case "triangle":
+      document.getElementById("triangle-par").style.display = 'block'
+      break;
+  }
+}
+
+circleBtn.onclick = function(){
+  const radius = Number(document.getElementById("radius").value);
+  let area = Math.PI * radius**2;
+  document.getElementById("circle-result").textContent = `Area of the circle is ${area}`;
+}
+
+squareBtn.onclick = function(){
+  const side = Number(document.getElementById("side-a").value)
+  let area = side**2;
+  document.getElementById("square-result").textContent = `Area of the square is ${area}`;
+}
+
+rectangleBtn.onclick = function(){
+  const sideA = Number(document.getElementById("rectangle-side-a").value);
+  const sideB = Number(document.getElementById("side-b").value);
+  let area = sideA * sideB;
+  document.getElementById("rectangle-result").textContent = `Area of the rectangle is ${area}`;
+}
+
+triangleBtn.onclick = function(){
+  const sideA = Number(document.getElementById("side-a").value);
+  const height = Number(document.getElementById("height").value);
+  let area = (sideA * height) / 2;
+  document.getElementById("triangle-result").textContent = `Area of the triangle is ${area}`;
+}
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const value = document.getElementById("count");
+let count = 0;
+
+increaseBtn.onclick = function(){
+  count++;
+  value.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+  count--;
+  value.textContent = count;
+}
+
+resetBtn.onclick = function(){
+  count = 0;
+  value.textContent = count;
 }
