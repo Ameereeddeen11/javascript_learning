5379e761bd3f7a88619a31b8478af430c94fc399
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-04-09 18:38:53 +0200

    editing main pag

--- index.js
+++ index.js
@@ -42,49 +42,6 @@ function validation(wages, dph){
   }
 }
 
-function select(){
-  switch (document.getElementById("objects").value){
-    case "circle":
-      document.getElementById("circle-par").style.display = 'block'
-      break;
-    case "square":
-      document.getElementById("square-par").style.display = 'block'
-      break;
-    case "rectangle":
-      document.getElementById("rectangle-par").style.display = 'block'
-      break;
-    case "triangle":
-      document.getElementById("triangle-par").style.display = 'block'
-      break;
-  }
-}
-
-circleBtn.onclick = function(){
-  const radius = Number(document.getElementById("radius").value);
-  let area = Math.PI * radius**2;
-  document.getElementById("circle-result").textContent = `Area of the circle is ${area}`;
-}
-
-squareBtn.onclick = function(){
-  const side = Number(document.getElementById("side-a").value)
-  let area = side**2;
-  document.getElementById("square-result").textContent = `Area of the square is ${area}`;
-}
-
-rectangleBtn.onclick = function(){
-  const sideA = Number(document.getElementById("rectangle-side-a").value);
-  const sideB = Number(document.getElementById("side-b").value);
-  let area = sideA * sideB;
-  document.getElementById("rectangle-result").textContent = `Area of the rectangle is ${area}`;
-}
-
-triangleBtn.onclick = function(){
-  const sideA = Number(document.getElementById("side-a").value);
-  const height = Number(document.getElementById("height").value);
-  let area = (sideA * height) / 2;
-  document.getElementById("triangle-result").textContent = `Area of the triangle is ${area}`;
-}
-
 const decreaseBtn = document.getElementById("decreaseBtn"); 
 const increaseBtn = document.getElementById("increaseBtn");
 const resetBtn = document.getElementById("resetBtn");
