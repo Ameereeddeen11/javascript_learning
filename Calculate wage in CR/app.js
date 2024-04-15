88392643dfe873cc06925b03b40b9dbd7d5a701c
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-04-15 19:21:52 +0200

    improved calculation of wage

--- Calculate wage in CR/app.js
+++ Calculate wage in CR/app.js
@@ -1,31 +1,32 @@
 var child;
 
-function childs(){
-  if (document.getElementById("yes").checked) {
-    document.getElementById("child").style.display = "block";
-    child = true;
-    countOfChilds = Number(document.getElementById("countOfChilds").value);
-  }
-  else {
-    child = false;
-  }
+function childs() {
+  document.getElementById("child").style.display = "block";
+  child = true;
 }
 
 document.getElementById("submit").onclick = function() {
     wages = Number(document.getElementById("wages").value);
     const dph = Number(document.getElementById("dph").value);
-    const insurance = 0.071;
     if (validation(wages, dph) == false){
       document.getElementById("result").textContent = "Please enter valid values!"
     }
     else{
-      income = wages - (wages * dph/100) - (wages * insurance);
-      document.getElementById("result").textContent = `Your income is ${income} !`;
+      calculate(wages, dph, child)
     }
   }
 
 function calculate(wages, dph, child) {
-
+  const insurance = 0.071;
+  let countOfChildren = Number(document.getElementById("countOfChildren").value);
+  if (child == true) {
+    income = wages - (wages * dph/100) - (wages * insurance) + (1267*countOfChildren);
+    document.getElementById("result").textContent = `Your income is ${income} !`;
+  }
+  else {
+    income = wages - (wages * dph/100) - (wages * insurance);
+    document.getElementById("result").textContent = `Your income is ${income} !`;
+  }
 }
   
 function validation(wages, dph){
