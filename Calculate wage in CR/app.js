0239eedc5422962c96655940fd8267681da83b33
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-04-15 20:18:30 +0200

    add graph to the project by using Chart

--- Calculate wage in CR/app.js
+++ Calculate wage in CR/app.js
@@ -7,26 +7,47 @@ function childs() {
 
 document.getElementById("submit").onclick = function() {
     wages = Number(document.getElementById("wages").value);
-    const dph = Number(document.getElementById("dph").value);
     if (validation(wages, dph) == false){
       document.getElementById("result").textContent = "Please enter valid values!"
     }
     else{
-      calculate(wages, dph, child)
+      calculate(wages, child)
     }
   }
 
-function calculate(wages, dph, child) {
+function calculate(wages, child) {
   const insurance = 0.071;
+  const dph = Number(document.getElementById("dph").value);
   let countOfChildren = Number(document.getElementById("countOfChildren").value);
   if (child == true) {
-    income = wages - (wages * dph/100) - (wages * insurance) + (1267*countOfChildren);
-    document.getElementById("result").textContent = `Your income is ${income} !`;
+    wage_dph = wages * dph/100;
+    wage_insurance = wages * insurance;
+    income = wages - wage_dph - wage_insurance + (1267*countOfChildren);
+    document.getElementById("result").textContent = `Your income is ${income} CZK !`;
   }
   else {
     income = wages - (wages * dph/100) - (wages * insurance);
-    document.getElementById("result").textContent = `Your income is ${income} !`;
+    document.getElementById("result").textContent = `Your income is ${income} CZK !`;
   }
+  var xValues = ["Income", "DPH", "Insurance"];
+  var yValues = [income, wage_dph, wage_insurance];
+  var pieColor = ["#3e95cd", "#8e5ea2", "#3cba9f"];
+  new Chart("myChart", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: pieColor,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Your income"
+      }
+    }
+  });
 }
   
 function validation(wages, dph){
@@ -38,4 +59,8 @@ function validation(wages, dph){
   else{
     return false;
   }
+}
+
+function graph(income, wage_dph, wage_insurance) {
+  
 }
